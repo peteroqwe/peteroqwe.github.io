3f00237a8cbeff7ac417ee5b40634973315ff86c
peteroqwe <112733544+peteroqwe@users.noreply.github.com>
2022-10-26 05:35:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function download() {
     var pdf = new jsPDF('p', 'pt', 'letter');
     pdf.canvas.height = 72 * 11;
     pdf.canvas.width = 72 * 8.5;
-    pdf.fromHTML(newWin.body);
+    pdf.fromHTML(newWin);
     
     setTimeout(function () { pdf.save('test.pdf'); }, 1000);
 }
